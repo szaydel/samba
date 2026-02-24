2778bdb33080e5ac0b9a0560246280278247db01
Volker Lendecke <vl@samba.org>
2026-02-24 09:44:49 +0100

    pam_winbind: Simplify _pam_compose_pwd_restriction_string
    Avoid explicit NULL handling with talloc_asprintf_addbuf()
    
    Signed-off-by: Volker Lendecke <vl@samba.org>
    Reviewed-by: Martin Schwenke <martin@meltin.net>

--- nsswitch/pam_winbind.c
+++ nsswitch/pam_winbind.c
@@ -1534,57 +1534,37 @@ static char *_pam_compose_pwd_restriction_string(struct pwb_context *ctx,
 	char *str = NULL;
 
 	if (!i) {
-		goto failed;
+		return NULL;
 	}
 
 	str = talloc_asprintf(ctx, _("Your password "));
-	if (!str) {
-		goto failed;
-	}
 
 	if (i->min_length_password > 0) {
-		str = talloc_asprintf_append(str,
-			       _("must be at least %d characters; "),
-			       i->min_length_password);
-		if (!str) {
-			goto failed;
-		}
+		talloc_asprintf_addbuf(&str,
+				       _("must be at least %d characters; "),
+				       i->min_length_password);
 	}
 
 	if (i->password_history > 0) {
-		str = talloc_asprintf_append(str,
-			       _("cannot repeat any of your previous %d "
-			        "passwords; "),
-			       i->password_history);
-		if (!str) {
-			goto failed;
-		}
+		talloc_asprintf_addbuf(&str,
+				       _("cannot repeat any of your previous "
+					 "%d passwords; "),
+				       i->password_history);
 	}
 
 	if (i->password_properties & WBC_DOMAIN_PASSWORD_COMPLEX) {
-		str = talloc_asprintf_append(str,
-			       _("must contain capitals, numerals "
-			         "or punctuation; "
-			         "and cannot contain your account "
-			         "or full name; "));
-		if (!str) {
-			goto failed;
-		}
+		talloc_asprintf_addbuf(&str,
+				       _("must contain capitals, numerals "
+					 "or punctuation; "
+					 "and cannot contain your account "
+					 "or full name; "));
 	}
 
-	str = talloc_asprintf_append(str,
-		       _("Please type a different password. "
-		         "Type a password which meets these requirements in "
-		         "both text boxes."));
-	if (!str) {
-		goto failed;
-	}
-
+	talloc_asprintf_addbuf(&str,
+			       _("Please type a different password. "
+				 "Type a password which meets these "
+				 "requirements in both text boxes."));
 	return str;
-
- failed:
-	TALLOC_FREE(str);
-	return NULL;
 }
 
 static int _pam_create_homedir(struct pwb_context *ctx,
