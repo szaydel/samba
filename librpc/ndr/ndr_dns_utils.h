82db1b8444162d82ec9277a6daab0f5f9060d5cc
Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
2026-04-02 09:35:01 +1300

    ndr:dns_utils.h: add header guards
    Signed-off-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>
    Reviewed-by: Andreas Schneider <asn@samba.org>
    
    Autobuild-User(master): Douglas Bagnall <dbagnall@samba.org>
    Autobuild-Date(master): Thu Apr 16 01:57:42 UTC 2026 on atb-devel-224

--- librpc/ndr/ndr_dns_utils.h
+++ librpc/ndr/ndr_dns_utils.h
@@ -1,4 +1,7 @@
 
+#ifndef _LIBRPC_NDR_NDR_DNS_UTILS_H
+#define _LIBRPC_NDR_NDR_DNS_UTILS_H
+
 enum ndr_err_code ndr_pull_dns_string_list(struct ndr_pull *ndr,
 					   ndr_flags_type ndr_flags,
 					   const char **s,
@@ -9,3 +12,5 @@ enum ndr_err_code ndr_push_dns_string_list(struct ndr_push *ndr,
 					   ndr_flags_type ndr_flags,
 					   const char *s,
 					   bool is_nbt);
+
+#endif
