6c34092c76e026759e33f5425176c1b51706c10c
Martin Schwenke <mschwenke@ddn.com>
2026-04-09 12:15:33 +1000

    ctdb-scripts: Only warn when removing an unassigned public IP
    get_iface_ip_maskbits() now sets iface="" when the IP is unassigned,
    allowing dependent code to be conditional.
    
    Currently, ctdb_takeover.c:ctdb_control_release_ip() ensures no
    releaseip event is triggered if the public address is not on the node.
    So, no change of behaviour for releaseip.
    
    The previous attempt at making updateip behave more like takeip when
    the IP isn't currently assigned caused commands with missing mask bits
    to be run.  Avoid this.
    
    Best reviewed with "git show -w" or similar.
    
    Signed-off-by: Martin Schwenke <mschwenke@ddn.com>
    Reviewed-by: John Mulligan <jmulligan@redhat.com>

--- ctdb/tests/UNIT/eventscripts/10.interface.releaseip.002.sh
+++ ctdb/tests/UNIT/eventscripts/10.interface.releaseip.002.sh
@@ -9,6 +9,6 @@ setup
 public_address=$(ctdb_get_1_public_address)
 ip="${public_address% *}" ; ip="${ip#* }"
 
-required_result 1 "ERROR: Unable to determine interface for IP ${ip}"
+ok "WARNING: Unable to determine interface for IP ${ip}"
 
 simple_test $public_address
