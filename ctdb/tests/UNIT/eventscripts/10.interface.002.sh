8c8dded9481cae424df0205dd06bfc49014dcf63
Martin Schwenke <mschwenke@ddn.com>
2026-04-13 14:48:07 +1000

    ctdb-tests: Add addip/releaseip altname unit test
    This shows that a warning is generated whenever an IP address is
    removed using an altname.
    
    Signed-off-by: Martin Schwenke <mschwenke@ddn.com>
    Reviewed-by: John Mulligan <jmulligan@redhat.com>

---
+++ ctdb/tests/UNIT/eventscripts/10.interface.002.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+. "${TEST_SCRIPTS_DIR}/unit.sh"
+
+define_test "Add/release 1 IP, interface is altname"
+
+setup
+
+ctdb_get_1_public_address |
+	while read -r dev ip bits; do
+		altname="alt${dev}"
+		ip link property add dev "$dev" altname "$altname"
+
+		ok_null
+		simple_test_event "takeip" "$altname" "$ip" "$bits"
+
+		ok <<EOF
+WARNING: Public IP ${ip} hosted on interface ${dev} but VNN says ${altname}
+EOF
+		simple_test_event "releaseip" "$altname" "$ip" "$bits"
+	done
